34ed0103f051c5fadddd35035950630b5dac1881
Lucas Martins Cardozo <cardozofixe@gmail.com>
2022-09-22 20:47:17 -0300

    Exercício para fixação de polimorfismo e herança

---
+++ Exercicio heranca e polimorfismo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Exercicio_heranca_e_polimorfismo.Entities;
+
+namespace Exercicio_heranca_e_polimorfismo {
+    class Program {
+        static void Main(string[] args) {
+
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int employees = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= employees; i++) {
+                Console.WriteLine($"Employee #{i} data: ");
+                Console.Write("Outsourced (y/n)? ");
+                char option = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (option == 'y') {
+
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+
+                }
+                else {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+
+            foreach (Employee emp in list) {
+                Console.WriteLine(emp.Name + " - $" + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
